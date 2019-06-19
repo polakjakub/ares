0164e7c5da1202a25ab4ef5a888e24ad677f7a46
Jakub Polák <polakjakub@gmail.com>
2019-06-20 00:33:04 +0200

    vypěkněné Bootstrapem verze 2

--- index.php
+++ index.php
@@ -98,35 +98,45 @@ $ico = isset($_REQUEST['ico']) ? $_REQUEST['ico'] : false;
 <head>
 	<meta charset="UTF-8">
 	<title>Příklad na ARES</title>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container center_div">
 
 <form>
-	<fieldset>
+	<fieldset class="form-group">
 		<legend>Zadej IČ:</legend>
-		<input type="text" name="ico" value="<?php echo htmlspecialchars($ico) ?>">
-		<input type="submit">
+		<input type="text" name="ico" value="<?php echo htmlspecialchars($ico) ?>" class="form-control">
 	</fieldset>
+	<input type="submit" class="btn btn-primary">
 </form>
 
 <?php	if ($ico): ?>
+	<hr class="col-xs-12">
 <?php
 	try {
 		$firma = getData($ico);
 
-		echo '<dl>';
-		echo '<dt>IČO</dt><dd>' . $firma->getICO() . '</dd>';
-		echo '<dt>Název</dt><dd>' . $firma->getNazev() . '</dd>';
-		echo '<dt>Adresa</dt><dd>' . $firma->getAdresa() . '</dd>';
-		echo '<dt>Obec</dt><dd>' . $firma->getObec() . '</dd>';
-		echo '<dt>PSČ</dt><dd>' . $firma->getPSC() . '</dd>';
+		
+		echo '<dl class="row">';
+		echo '<dt class="col-sm-3">IČO</dt><dd class="col-sm-9">' . $firma->getICO() . '</dd>';
+		echo '<dt class="col-sm-3">Název</dt><dd class="col-sm-9">' . $firma->getNazev() . '</dd>';
+		echo '<dt class="col-sm-3">Adresa</dt><dd class="col-sm-9">' . $firma->getAdresa() . '</dd>';
+		echo '<dt class="col-sm-3">Obec</dt><dd class="col-sm-9">' . $firma->getObec() . '</dd>';
+		echo '<dt class="col-sm-3">PSČ</dt><dd class="col-sm-9">' . $firma->getPSC() . '</dd>';
 		echo '</dl>';
 		
 	} catch (Exception $e) {
-		echo '<p style="color:red">' . $e->getMessage() . '</p>';
+		echo '<p class="alert alert-danger" role="alert">' . $e->getMessage() . '</p>';
 	}
 ?>
 <?php endif; ?>
+</div>
 
 </body>
 </html>
