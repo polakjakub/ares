1c0cb4ed4b74e8fb1a5616e6c2d9aff5d4d109b4
Jakub Polák <polakjakub@gmail.com>
2019-06-20 10:31:21 +0200

    ajax autoloader, verze 4

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
 
 	define('CACHE_DIR', realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'cache'));
 	
@@ -100,18 +99,109 @@ function validateICO($ico)
 }
 	
 $ico = isset($_REQUEST['ico']) ? $_REQUEST['ico'] : false;
-
+	
+// zde se posila JSON pro autoloader
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+	header('Content-Type: application/json');
+	
+	try {
+		$firma = getData($ico);
+		die(json_encode([
+			'status' => 'success',
+			'data' => [
+				'ICO' => $firma->getICO(),
+				'nazev' => $firma->getNazev(),
+				'adresa' => $firma->getAdresa(),
+				'obec' => $firma->getObec(),
+				'PSC' => $firma->getPSC(),
+			]
+		]));
+	} catch (Exception $e) {
+		die(json_encode([
+			'status' => 'failed',
+			'message' => $e->getMessage(),
+		]));
+	}
+	die(json_encode([]));
+}
 ?><!DOCTYPE html>
 <html lang="cs">
 <head>
 	<meta charset="UTF-8">
 	<title>Příklad na ARES</title>
 	
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script
+		src="https://code.jquery.com/jquery-3.4.1.min.js"
+		integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+		crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+	
+	<script>
+		var aresCalls = (function($){
+			var
+				aresLoad,
+				printData,
+				printError,
+				init;
+			
+			printData = function (ICO, nazev, adresa, obec, PSC) {
+				$('#aresResults').html('<hr class="col-xs-12"><dl class="row">' +
+					'<dt class="col-sm-3">IČO</dt><dd class="col-sm-9">' + ICO + '</dd>' +
+					'<dt class="col-sm-3">Název</dt><dd class="col-sm-9">' + nazev + '</dd>' +
+					'<dt class="col-sm-3">Adresa</dt><dd class="col-sm-9">' + adresa + '</dd>' +
+					'<dt class="col-sm-3">Obec</dt><dd class="col-sm-9">' + obec + '</dd>' +
+					'<dt class="col-sm-3">PSČ</dt><dd class="col-sm-9">' + PSC + '</dd>' +
+					'</dl>');
+			}
+			
+			printError = function (message) {
+				$('#aresResults').html('<hr class="col-xs-12"><p class="alert alert-danger" role="alert">' + message + '</p>');
+				
+			}
+			
+			aresLoad = function () {
+				if ($('#ico').val().length == 8) {
+					$.ajax({
+						url: "<?php echo $_SERVER['SCRIPT_URI'] ?>",
+						data: "ico=" + $('#ico').val(),
+						cache: true,
+						success: function (data) {
+							if (data.status == 'success') {
+								printData(data.data.ICO, data.data.nazev, data.data.adresa, data.data.obec, data.data.PSC)
+							} else if (data.status == 'failed') {
+								printError(data.message);
+							} else {
+								$('#aresResults').html('');
+							}
+						},
+					});
+				} else if ($('#ico').val().length > 6) {
+					printError('IČO musí mít 8 číslic.');
+				} else {
+					$('#aresResults').html('');
+				}
+			};
+			
+			init = function(){
+				$(document).on('keyup', '#ico', aresLoad);
+			};
+			
+			$(document).ready(init);
+			
+			return {
+				aresLoad: aresLoad,
+				printData: printData,
+				printError: printError,
+			};
+			
+		}($));
+	
+	</script>
+
 </head>
 <body>
 <div class="container center_div">
@@ -119,31 +209,22 @@ $ico = isset($_REQUEST['ico']) ? $_REQUEST['ico'] : false;
 <form>
 	<fieldset class="form-group">
 		<legend>Zadej IČ:</legend>
-		<input type="text" name="ico" value="<?php echo htmlspecialchars($ico) ?>" class="form-control">
+		<input type="text" name="ico" id="ico" value="<?php echo htmlspecialchars($ico) ?>" class="form-control">
 	</fieldset>
 	<input type="submit" class="btn btn-primary">
 </form>
 
-<?php	if ($ico): ?>
-	<hr class="col-xs-12">
-<?php
+	<div id="aresResults">
+
+<?php	if ($ico):
 	try {
 		$firma = getData($ico);
-
-		
-		echo '<dl class="row">';
-		echo '<dt class="col-sm-3">IČO</dt><dd class="col-sm-9">' . $firma->getICO() . '</dd>';
-		echo '<dt class="col-sm-3">Název</dt><dd class="col-sm-9">' . $firma->getNazev() . '</dd>';
-		echo '<dt class="col-sm-3">Adresa</dt><dd class="col-sm-9">' . $firma->getAdresa() . '</dd>';
-		echo '<dt class="col-sm-3">Obec</dt><dd class="col-sm-9">' . $firma->getObec() . '</dd>';
-		echo '<dt class="col-sm-3">PSČ</dt><dd class="col-sm-9">' . $firma->getPSC() . '</dd>';
-		echo '</dl>';
-		
+		printf('<script> aresCalls.printData("%d", "%s", "%s", "%s", "%d"); </script>', $firma->getICO(), addslashes($firma->getNazev()), addslashes($firma->getAdresa()), addslashes($firma->getObec()), $firma->getPSC());
 	} catch (Exception $e) {
-		echo '<p class="alert alert-danger" role="alert">' . $e->getMessage() . '</p>';
+		printf('<script> aresCalls.printError("%s"); </script>', $e->getMessage());
 	}
-?>
-<?php endif; ?>
+endif; ?>
+	</div>
 </div>
 
 </body>
